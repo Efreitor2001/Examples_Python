0a6264824da79fb7262e65bfc6721b7e7cff41fe
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-05 20:53:42 +0300

    Lection_001

---
+++ HelloPython/lec.py
@@ -0,0 +1,91 @@
+# value = None
+# print(type(value))
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 1234
+# s = "hello world"
+# print(s) # Вывод строки
+# print(a,b,s)
+# print(f'{a} - {b} - {s}')
+# print('{1} - {2} - {0}'.format(a,b,s))
+# f = True
+# print(f)
+# list = ['1','2','3']
+# col = ['hello',1,2,3,4,True]
+# print(list)
+# print(col)
+# print('Введите а')
+# a = int(input())
+# print('Введите b')
+# b = int(input())
+# print(a,' + ',b,' = ', a+b)
+
+# a = 1.3123
+# b = 3
+# c = round(a*b,5)
+# print(c)
+# a = 3
+# a += 5
+# print(a)
+
+# a = 1 < 3 < 5 < 10 > 12
+# print(a)
+
+# func = 1
+# T = 4
+# x = 2
+# print(func<T>(x))
+
+# f = 1 > 2 or 4 < 6
+# print(f)
+
+# f = [1,2,3,4]
+# print(f)
+# print(not 6 in f)
+
+# is_odd = not f[3] % 2
+# print(is_odd)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('1')
+# elif username == 'Dima':
+#     print('2')
+# else:
+#     print('Hello,', username)
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+#     print(original)
+# else:
+#     print('Stop')
+#     print('it )')
+# print(inverted)
+
+
+# for i in 'qwerty':
+#     print(i)
+
+# def  f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+
+# arg = 2
+# print(f(arg))
+# print(type(f(arg)))
